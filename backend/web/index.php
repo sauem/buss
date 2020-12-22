f0d73adbd5061d6586dabfe363f538504f8e28b1
sauem <nguyendinhthang.go97@gmail.com>
2020-12-22 20:42:59 +0700

    dadas

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
