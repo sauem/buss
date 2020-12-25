4d5abcfe84d5f8ff546baca70503822a69151594
sauem <nguyendinhthang.go97@gmail.com>
2020-12-25 22:20:26 +0700

    dadas

--- backend/web/theme/js/banner.ads.js
+++ backend/web/theme/js/banner.ads.js
@@ -158,7 +158,7 @@ function initAds(page = 'home') {
             let lux_event = $("body").find("section.lux_event");
             let position = Math.round(lux_event.length / 2) - 1;
             console.log(position);
-            $("body").find(`section.lux_event:nth-child(${position})`).append(sticky);
+            $(`section.lux_event:nth-child(${position})`).append(sticky);
         }
 
     }
